7e8976c5fa16e32ff5edd43d1ac1f29f83267a1b
KorchinskiyIlya <korchinskiy.ilya@gmail.com>
2019-07-28 11:44:27 +0300

    servlets

---
+++ JavaEEApp/src/main/java/ru/korchinskiy/servlet/OrderServlet.java
@@ -0,0 +1,29 @@
+package ru.korchinskiy.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/order")
+public class OrderServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Order</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Order</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+
+    }
+}
